36342d6cc03ee197851f2fc1cb58a0eb58f79a93
Mui <m.samita.tt@gmail.com>
2024-02-21 14:07:03 +0700

    feat: finished exercise

--- top2-max-numbers/index.mjs
+++ top2-max-numbers/index.mjs
@@ -8,4 +8,22 @@
 // getTop2MaxNumbers([-10, -40, -20, -2, -9, -19]) === [-2, -9]
 // getTop2MaxNumbers([10, 40, 20, 2, 40, 9, 19]) === [40, 20]
 
-export const getTop2MaxNumbers = (numbers) => {};
+export const getTop2MaxNumbers = (numbers) => {
+  let highestNumber = -Infinity;
+  let secondHighestNumber = -Infinity;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > highestNumber) {
+      highestNumber = numbers[i];
+    } else if (
+      numbers[i] > secondHighestNumber &&
+      numbers[i] !== highestNumber
+    ) {
+      secondHighestNumber = numbers[i];
+    }
+  }
+  return [highestNumber, secondHighestNumber];
+};
+
+console.log(getTop2MaxNumbers([10, 40, 20, 2, 9, 19]));
+console.log(getTop2MaxNumbers([-10, -40, -20, -2, -9, -19]));
+console.log(getTop2MaxNumbers([10, 40, 20, 2, 40, 9, 19]));
